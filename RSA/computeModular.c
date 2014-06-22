272cbe3de8f2200765a45ede88cd9bec5704f749
Steffen Tröster <stetro@steffens-air.fritz.box>
2014-06-22 17:16:02 +0200

    adding computeRSA.c and a proper Makefile

--- RSA/computeModular.c
+++ RSA/computeModular.c
@@ -1,8 +1,9 @@
-/* computeModular.c
-	Example Code for Lab 3 
-	requires libssl libcrpypto
-	Ubuntu: Package libssl-dev required
-	Compilation: gcc computeModular.c -o computeModular -lssl -lcrypto 
+/*
+   computeModular.c
+   Example Code for Lab 3 
+   requires libssl libcrpypto
+Ubuntu: Package libssl-dev required
+Compilation: gcc computeModular.c -o computeModular -lssl -lcrypto 
 */
 
 #include <stdio.h>
@@ -23,76 +24,76 @@ int main(int argc, char **argv)
 	BIGNUM *b;
 	BIGNUM *c;
 	BIGNUM *n;
-    BIGNUM *p;
+	BIGNUM *p;
 	BN_CTX *t;
 	char input_a[MAXDIGITS];
 	char input_b[MAXDIGITS];
 	char input_n[MAXDIGITS];
-    char seed[MAXDIGITS];
+	char seed[MAXDIGITS];
 	struct timeval start, end;
-	
+
 	t = BN_CTX_new();
 	BN_CTX_init(t);
-	
+
 	a = BN_new();
 	b = BN_new();
 	c = BN_new();
 	n = BN_new();
-    p = BN_new();
-	
-    printf("Generate Random Prime p  \n ******  \n");
-    
-    // Seed
-    printf("Input Seed :  ");
-    fgets(seed, MAXDIGITS,stdin);
-    
-    RAND_seed(seed, strnlen(seed,MAXDIGITS));
-    
-    // Generate random prime
-    
-    BN_generate_prime_ex(p, NUMBITS, 0, NULL, NULL,NULL);
-    
-    printf("p = %s\n ****** \n",BN_bn2dec(p));
-    
-    printf("Modular exponentiation a^b mod n  \n ******  \n");
+	p = BN_new();
 
-    // Input a, b, n
-    
+	printf("Generate Random Prime p  \n ******  \n");
+
+	// Seed
+	printf("Input Seed :  ");
+	fgets(seed, MAXDIGITS,stdin);
+
+	RAND_seed(seed, strnlen(seed,MAXDIGITS));
+
+	// Generate random prime
+
+	BN_generate_prime_ex(p, NUMBITS, 0, NULL, NULL,NULL);
+
+	printf("p = %s\n ****** \n",BN_bn2dec(p));
+
+	printf("Modular exponentiation a^b mod n  \n ******  \n");
+
+	// Input a, b, n
+
 	printf("a=  ");
 	fgets(input_a, MAXDIGITS,stdin); // includes \n
 
-    
+
 	printf("b=  ");
 	fgets(input_b, MAXDIGITS,stdin); // includes \n
 
-	
+
 	printf("n=  ");
 	fgets(input_n, MAXDIGITS,stdin); // includes \n
 
 	// char arrays to BIGNUM
-	
+
 	BN_dec2bn(&a,input_a);
 	BN_dec2bn(&b,input_b);
 	BN_dec2bn(&n,input_n);
-	
+
 	gettimeofday(&start, NULL); // start time
-    
-    // c=a^b mod n
-    
+
+	// c=a^b mod n
+
 	BN_mod_exp(c,a,b,n,t);
-    
-    if (ERR_get_error() != 0) {
-        printf("Error ! \n");
-        exit(1);
-    }
-    
-    
+
+	if (ERR_get_error() != 0) {
+		printf("Error ! \n");
+		exit(1);
+	}
+
+
 	gettimeofday(&end, NULL);   // end time
-    
-    
+
+
 	printf("%s^%s mod %s = %s\n",BN_bn2dec(a),BN_bn2dec(b),BN_bn2dec(n), BN_bn2dec(c));
-    
+
 	printf("Time elapsed: %ld Microseconds  \n", (long) end.tv_usec - start.tv_usec); 
-    
+
 	return(0);
 }
